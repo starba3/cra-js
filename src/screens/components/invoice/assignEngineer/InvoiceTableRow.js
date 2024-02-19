6aaedc1719d833bb0b107a7c769d7e45279bef76
Mustafa Alsamarrai <starba.ma@gmail.com>
2024-02-19 15:09:24 +0300

    Assign engineeer incomplete

---
+++ src/screens/components/invoice/assignEngineer/InvoiceTableRow.js
@@ -0,0 +1,154 @@
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import { useLocales } from 'src/locales';
+// @mui
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import MenuItem from '@mui/material/MenuItem';
+import TableRow from '@mui/material/TableRow';
+import Checkbox from '@mui/material/Checkbox';
+import TableCell from '@mui/material/TableCell';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ListItemText from '@mui/material/ListItemText';
+// hooks
+import { useBoolean } from 'src/hooks/use-boolean';
+// utils
+// components
+import Iconify from 'src/components/iconify';
+import { ConfirmDialog } from 'src/components/custom-dialog';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
+
+// ----------------------------------------------------------------------
+
+export default function InvoiceTableRow({
+  row,
+  selected,
+  onViewRow,
+  handleOpenInquiry,
+}) {
+  const { id, invoiceNo, issueInvoiceDate, invoiceAmount, installationStatus, customerNameEn, customerNameAr, productNameEn, productNameAr, department } = row;
+
+  const { t, currentLang } = useLocales()
+
+  const customerName = currentLang.value === 'ar' ? customerNameAr : customerNameEn;
+  const productName = currentLang.value === 'ar' ? productNameAr : productNameEn;
+
+  const Translate = (text) => t(text);
+
+  const confirm = useBoolean();
+
+  const popover = usePopover();
+
+  return (
+    <>
+      <TableRow hover selected={selected}>
+
+        <TableCell sx={{ display: 'flex', alignItems: 'center' }}>
+          <Avatar alt={customerName} sx={{ mr: 2 }}>
+            {customerName.charAt(0).toUpperCase()}
+          </Avatar>
+
+          <ListItemText
+            disableTypography
+            primary={
+              <Typography variant="body2" noWrap>
+                {customerName}
+              </Typography>
+            }
+            secondary={
+              <Link
+                noWrap
+                variant="body2"
+                onClick={onViewRow}
+                sx={{ color: 'text.disabled', cursor: 'pointer' }}
+              >
+                {invoiceNo}
+              </Link>
+            }
+          />
+        </TableCell>
+
+        <TableCell>
+          <ListItemText
+            primary={format(new Date(issueInvoiceDate), 'dd MMM yyyy')}
+            secondary={format(new Date(issueInvoiceDate), 'p')}
+            primaryTypographyProps={{ typography: 'body2', noWrap: true }}
+            secondaryTypographyProps={{
+              mt: 0.5,
+              component: 'span',
+              typography: 'caption',
+            }}
+          />
+        </TableCell>
+        
+        <TableCell align="center" >{installationStatus}</TableCell>
+
+        <TableCell >{`${invoiceAmount.toLocaleString()} ${Translate('currencyShortcut')}`}</TableCell>
+
+        <TableCell align="center" >{productName}</TableCell>
+
+        <TableCell align="center" >{department}</TableCell>
+
+        <TableCell>
+          <Button 
+            variant='contained' 
+            color='success'
+            onClick={() => handleOpen(id)}
+          >
+            {`${Translate("assign")} ${Translate("engineer")}`} 
+          </Button>
+        </TableCell>
+
+         <TableCell align="right" sx={{ px: 1 }}>
+          <IconButton color={popover.open ? 'inherit' : 'default'} onClick={popover.onOpen}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton>
+        </TableCell> 
+
+        
+      </TableRow>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 160 }}
+      >
+        <MenuItem
+          onClick={() => {
+            onViewRow();
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:eye-bold" />
+          {Translate("view")}
+        </MenuItem>
+
+        
+        <MenuItem
+          onClick={() => {
+            handleOpenInquiry();
+            // confirm.onTrue();
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="bx:file" />
+          {Translate("inquiry")}
+        </MenuItem>
+
+        {/* <Divider sx={{ borderStyle: 'dashed' }} /> */}
+
+      </CustomPopover>
+    </>
+  );
+}
+
+InvoiceTableRow.propTypes = {
+
+  onViewRow: PropTypes.func,
+  handleOpenInquiry: PropTypes.func,
+  row: PropTypes.object,
+  selected: PropTypes.bool,
+};
---
+++ src/screens/components/invoice/assignEngineer/InvoiceTableToolbar.js
@@ -0,0 +1,150 @@
+import PropTypes from 'prop-types';
+import { useCallback } from 'react';
+import { useLocales } from 'src/locales';
+// @mui
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import Stack from '@mui/material/Stack';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+// components
+import Iconify from 'src/components/iconify';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
+
+// ----------------------------------------------------------------------
+
+export default function InvoiceTableToolbar({
+  filters,
+  onFilters,
+  dateError,
+}) {
+  const popover = usePopover();
+
+  const { t } = useLocales()
+  const Translate = (text) => t(text);
+
+  const handleFilterName = useCallback(
+    (event) => {
+      onFilters('name', event.target.value);
+    },
+    [onFilters]
+  );
+
+  const handleFilterStartDate = useCallback(
+    (newValue) => {
+      onFilters('startDate', newValue);
+    },
+    [onFilters]
+  );
+
+  const handleFilterEndDate = useCallback(
+    (newValue) => {
+      onFilters('endDate', newValue);
+    },
+    [onFilters]
+  );
+
+  return (
+    <>
+      <Stack
+        spacing={2}
+        alignItems={{ xs: 'flex-end', md: 'center' }}
+        direction={{
+          xs: 'column',
+          md: 'row',
+        }}
+        sx={{
+          p: 2.5,
+          pr: { xs: 2.5, md: 1 },
+        }}
+      >
+        
+        <DatePicker
+          label={Translate("startDate")}
+          value={filters.startDate}
+          onChange={handleFilterStartDate}
+          slotProps={{ textField: { fullWidth: true } }}
+          sx={{
+            maxWidth: { md: 180 },
+          }}
+        />
+
+        <DatePicker
+          label={Translate("endDate")}
+          value={filters.endDate}
+          onChange={handleFilterEndDate}
+          slotProps={{
+            textField: {
+              fullWidth: true,
+              error: dateError,
+            },
+          }}
+          sx={{
+            maxWidth: { md: 180 },
+          }}
+        />
+
+        <Stack direction="row" alignItems="center" spacing={2} flexGrow={1} sx={{ width: 1 }}>
+          <TextField
+            fullWidth
+            value={filters.name}
+            onChange={handleFilterName}
+            placeholder={Translate("searchInvoiceNamePlaceHolder")}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled' }} />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          {/* <IconButton onClick={popover.onOpen}>
+            <Iconify icon="eva:more-vertical-fill" />
+          </IconButton> */}
+        </Stack>
+      </Stack>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:printer-minimalistic-bold" />
+          {Translate("print")}
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:import-bold" />
+          {Translate("import")}
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+        >
+          <Iconify icon="solar:export-bold" />
+          {Translate("export")}
+        </MenuItem>
+      </CustomPopover>
+    </>
+  );
+}
+
+InvoiceTableToolbar.propTypes = {
+  dateError: PropTypes.bool,
+  filters: PropTypes.object,
+  onFilters: PropTypes.func,
+};
